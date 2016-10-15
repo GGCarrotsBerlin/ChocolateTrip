90e6afa4dcddd0103d7b0aa6ab06c02656429be0
ouafae <ouafae.benkadi@gmail.com>
2016-10-15 23:08:21 +0200

    UI screens

---
+++ RunAway/Assets/_Scripts/SwitchToSetting.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SwitchToSetting : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (Timer ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void SwitchToScene(){
+		
+
+		SceneManager.LoadScene (1);
+	}
+
+	public IEnumerator Timer(){
+
+		yield return new WaitForSeconds (3);
+		SwitchToScene ();
+	}
+}
---
+++ RunAway/Assets/_Scripts/animateLogo.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class animateLogo : MonoBehaviour {
+
+	public Image logo;
+
+	// Use this for initialization
+	void Start () {
+		FadeIn (logo.gameObject);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void FadeIn(GameObject gameObject) {
+
+		iTween.ValueTo (gameObject, iTween.Hash (
+			"from", 0f, "to", 1f,
+			"time", 3f, "easetype", "linear",
+			"onupdate", "setAlpha"));
+
+			}
+
+}
